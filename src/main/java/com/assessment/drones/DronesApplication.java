130bbd83e4779e193ac22d18de310a2bf2d39c21
Ryan Christian <ryanchristian4427@gmail.com>
2018-11-15 14:05:29 +0000

    Empty directories

--- src/main/java/com/assessment/drones/DronesApplication.java
+++ src/main/java/com/assessment/drones/DronesApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DronesApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(DronesApplication.class, args);
