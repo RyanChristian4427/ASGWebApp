5288ef05ca1bf4a6a47d230fbdee88de646fbad7
James Apps <appsj@cardiff.ac.uk>
2018-11-20 15:15:47 +0000

    Made general table for database schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,6 +11,24 @@ CREATE TABLE IF NOT EXISTS `candidate` (
 )
 ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS `general` (
+	`id` INT NOT NULL,
+	`first_name` VARCHAR(20) NOT NULL,
+    `surname` VARCHAR(20) NOT NULL,
+    `date_of_birth` DATE NOT NULL,
+    `place_of_birth` VARCHAR(50) NOT NULL,
+    `address_id` INT NOT NULL,
+    `company_name` VARCHAR(20) NOT NULL,
+    `previous_flying_exp` TEXT NOT NULL,
+    `preferred_location` VARCHAR(50) NOT NULL,
+    `drone_type` VARCHAR(50) NOT NULL,
+    `candidate_number` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`id`),
+    UNIQUE (`candidate_number`),
+    FOREIGN KEY (`candidate_number`) REFERENCES `candidate` (`candidate_number`)
+) ENGINE=InnoDB;
+
 CREATE TABLE IF NOT EXISTS `review` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `candidate_number` INT UNSIGNED NOT NULL,
