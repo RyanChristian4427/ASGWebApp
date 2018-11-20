1bbbf04e32e056c6c0b0178a48a0ec7bdf9f752d
James Apps <appsj@cardiff.ac.uk>
2018-11-20 15:00:05 +0000

    Made candidate table for database schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,19 @@
 SET MODE MySQL;
 SET IGNORECASE=TRUE;
 
-CREATE TABLE IF NOT EXISTS `client` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `progress_id` VARCHAR(20) NOT NULL,
-  `basic_info_id` INT UNSIGNED NOT NULL,
-  `address_id` TEXT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS `venue` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `instructor_id` INT UNSIGNED NOT NULL,
-  `location_address_id` INT UNSIGNED NOT NULL,
-  `course_name` VARCHAR(20) NOT NULL,
-  `start_time` TIME NOT NULL,
-  `end_time` TIME NOT NULL,
-  `participants_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `candidate` (
+	`candidate_number` VARCHAR(20) NOT NULL,
+  `course_number` VARCHAR(50) NOT NULL,
+  `course_start` DATE NOT NULL,
+  `course_end` DATE NOT NULL,
+  PRIMARY KEY (`candidate_number`),
+  UNIQUE KEY (`candidate_number`)
+)
+ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `review` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `client_id` INT UNSIGNED NOT NULL,
+  `candidate_number` INT UNSIGNED NOT NULL,
   `instructor_id` INT UNSIGNED NOT NULL,
   `review_text` TEXT NOT NULL,
   PRIMARY KEY (`id`))
