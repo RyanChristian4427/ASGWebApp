753c4aed1c3d576bd213b40817104e351b3532c5
Ryan Christian <ryanchristian4427@gmail.com>
2018-11-28 15:10:40 +0000

    Working client dashboard, including logout ability

--- src/main/java/com/assessment/drones/controllers/DashboardController.java
+++ src/main/java/com/assessment/drones/controllers/DashboardController.java
@@ -1,22 +1,21 @@
 package com.assessment.drones.controllers;
 
-import com.assessment.drones.domain.LoginDto;
-import com.assessment.drones.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import java.security.Principal;
 
 @Controller
 public class DashboardController {
 
     //All of this is temp while waiting for the database to be created
     @RequestMapping(path = "/dashboard", method = RequestMethod.GET)
-    public String viewDashboard(@RequestParam(value = "user", required = false) String userEmail, Model model) {
-        return "dashboard";
+    public String viewDashboard(Principal principal, Model model) {
+        model.addAttribute("userName", principal.getName());
+        return "clientDashboard";
     }
 
 
