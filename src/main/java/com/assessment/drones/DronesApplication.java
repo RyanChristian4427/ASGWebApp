68cb208ad75380c3490d2120a33fffa0a32a7a11
James Apps <appsj@cardiff.ac.uk>
2018-12-04 11:06:54 +0000

    Flyway works

--- src/main/java/com/assessment/drones/DronesApplication.java
+++ src/main/java/com/assessment/drones/DronesApplication.java
@@ -2,6 +2,9 @@ package com.assessment.drones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class DronesApplication
@@ -9,5 +12,13 @@ public class DronesApplication
     public static void main(String[] args)
     {
         SpringApplication.run(DronesApplication.class, args);
+    }
+    @Bean
+    @Profile("development")
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+            flyway.clean();
+            flyway.migrate();
+        };
     }
 }
