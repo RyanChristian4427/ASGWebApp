a23340d47e408da4756502b6c35788598b380e91
Ryan Christian <ryanchristian4427@gmail.com>
2019-03-19 17:58:52 +0000

    Adding checkstyle plugin and rules

--- src/main/java/com/assessment/asg/services/interfaces/ReviewService.java
+++ src/main/java/com/assessment/asg/services/interfaces/ReviewService.java
@@ -2,8 +2,6 @@ package com.assessment.asg.services.interfaces;
 
 import com.assessment.asg.domain.ReviewDto;
 
-import java.util.List;
-
 public interface ReviewService {
 
     void addReview(ReviewDto reviewDto);
